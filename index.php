238b1c7aefc105d338d8e62abdc0eda5262be461
Eris <erisemanoelrs@gmail.com>
2024-12-01 21:41:33 -0300

    Finalização

--- index.php
+++ index.php
@@ -6,8 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Teste de calendário</title>
     <style>
-        td {
+        td,
+        th,
+        table {
             border: 1px solid black;
+            border-spacing: 0;
+            border-collapse: collapse;
         }
     </style>
 </head>
@@ -15,9 +19,11 @@
 <body>
     <main>
         <form method="get" action="index.php">
-            <input type="month" id="mes" name="mes">
+            <input type="month" id="mes" name="mes"
+                value="<?php echo isset($_GET['mes']) ? $_GET['mes'] : date('Y-m'); ?>">
             <input type="submit" value="Cadastrar">
         </form>
+
         <?php
         if (isset($_GET['mes'])) {
 
@@ -30,8 +36,7 @@
 
             $diassemana = date('w', strtotime($data));
 
-
-
+            echo "<h2>Calendário " . $data = date('Y', strtotime($data)) . "</h2>";
             echo "<table>";
             echo "<tr>
                     <th>Dom</th>
@@ -42,16 +47,33 @@
                     <th>Sex</th>
                     <th>Sab</th>
                     </tr>";
-
-
-
             echo "<tr>";
 
 
-            for ($i = 0; $i < $diassemana; $i++) {
 
+            for ($i = 0; $i < $diassemana; $i++) {
                 echo "<td>&nbsp;</td>";
             }
+
+            for ($i = 1; $i <= $diasmes; $i++) {
+                echo "<td>" . $i . "</td>";
+
+                if (($i + $diassemana) % 7 == 0) {
+                    echo "</tr><tr>";
+
+
+                }
+            }
+
+
+            for ($i = 0; $i < (7 - (($diasmes + $diassemana) % 7)); $i++) {
+                echo "<td>&nbsp;</td>";
+            }
+
+            echo "</tr>";
+            echo "</table>";
+
+
         }
         ?>
     </main>
